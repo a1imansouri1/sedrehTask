473c8d0f948080a1b4625df4acafd4292a183a92
Ali Mansouri <ali.mansouri0901@yahoo.com>
2023-03-11 09:48:13 +0330

    a

--- src/Pages/PlatformLaunch.jsx
+++ src/Pages/PlatformLaunch.jsx
@@ -7,7 +7,7 @@ const PlatformLaunch = () => {
   return (
     <>
       <Header title='Platform Launch'>
-        <Status title='TODO' color='rgb(34 211 238)' toDoQuant='(4)' />
+        <Status title='TODO' color='rgb(34 211 238)' toDoQuant='(1)' />
         <div className='ml-[300px]'>
           <Task title='Build UI for onboarding flow' description='0 of 3 substacks' />
         </div>
